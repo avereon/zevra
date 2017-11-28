26648191ecfaaea8467fb6187b289ce8be32b0cf
Mark Soderquist <mark@soderquist.net>
2017-11-27 19:40:53 -0700

    Continue work on update manager

--- source/test/java/com/xeomar/util/UriUtilTest.java
+++ source/test/java/com/xeomar/util/UriUtilTest.java
@@ -1,6 +1,6 @@
 package com.xeomar.util;
 
-import junit.framework.TestCase;
+import org.junit.Test;
 
 import java.io.File;
 import java.net.URI;
@@ -10,8 +10,28 @@ import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.nullValue;
 import static org.junit.Assert.assertThat;
 
-public class UriUtilTest extends TestCase {
+public class UriUtilTest {
 
+	@Test
+	public void understandUriParts() {
+		URI uri1 = URI.create( "program:product#updates" );
+		assertThat( uri1.getScheme(), is( "program" ) );
+		assertThat( uri1.getPath(), is( nullValue() ) );
+		assertThat( uri1.getQuery(), is( nullValue() ) );
+		assertThat( uri1.getFragment(), is( "updates" ) );
+		assertThat( uri1.getAuthority(), is( nullValue() ) );
+		assertThat( uri1.getSchemeSpecificPart(), is( "product" ) );
+
+		URI uri2 = URI.create( "http://xeomar.com/download/xenon/product/card?version=latest&refresh=false" );
+		assertThat( uri2.getScheme(), is( "http" ) );
+		assertThat( uri2.getAuthority(), is( "xeomar.com" ) );
+		assertThat( uri2.getPath(), is( "/download/xenon/product/card" ) );
+		assertThat( uri2.getQuery(), is( "version=latest&refresh=false" ) );
+		assertThat( uri2.getFragment(), is( nullValue() ) );
+		assertThat( uri2.getSchemeSpecificPart(), is( "//xeomar.com/download/xenon/product/card?version=latest&refresh=false" ) );
+	}
+
+	@Test
 	public void testResolveWithString() throws Exception {
 		assertThat( UriUtil.resolve( "" ), is( new File( "" ).getCanonicalFile().toURI() ) );
 		assertThat( UriUtil.resolve( "." ), is( new File( "." ).getCanonicalFile().toURI() ) );
@@ -21,6 +41,7 @@ public class UriUtilTest extends TestCase {
 		assertThat( UriUtil.resolve( "ssh://localhost" ), is( URI.create( "ssh://localhost" ) ) );
 	}
 
+	@Test
 	public void testResolveWithRelativeUri() {
 		URI base = URI.create( "file:/test/folder/" );
 		URI absolute = URI.create( "file:/test/folder/absolute" );
@@ -39,12 +60,14 @@ public class UriUtilTest extends TestCase {
 		assertThat( UriUtil.resolve( jar, relative ), is( URI.create( "jar:file:/test/folder%20with%20spaces/file.jar!/path/to/relative" ) ) );
 	}
 
+	@Test
 	public void testResolveWithAbsoluteUri() {
 		URI jar = URI.create( "jar:file:/test/folder%20with%20spaces/file.jar!/path/to/resource" );
 		URI icon = URI.create( "http://www.parallelsymmetry.com/images/icons/escape.png" );
 		assertThat( UriUtil.resolve( jar, icon ), is( URI.create( "http://www.parallelsymmetry.com/images/icons/escape.png" ) ) );
 	}
 
+	@Test
 	public void testGetParent() {
 		URI absolute = URI.create( "file:/test/folder/absolute" );
 		URI opaque = URI.create( "jar:" + absolute.toString() );
@@ -55,8 +78,9 @@ public class UriUtilTest extends TestCase {
 		assertThat( UriUtil.getParent( doubleOpaque ).toString(), is( "double:jar:file:/test/folder/" ) );
 	}
 
+	@Test
 	public void testParseQueryWithUri() {
-		assertNull( UriUtil.parseQuery( (URI)null ) );
+		assertThat( UriUtil.parseQuery( (URI)null ), is( nullValue() ) );
 
 		URI uri = URI.create( "test:///path?attr1&attr2" );
 		Map<String, String> parameters = UriUtil.parseQuery( uri );
@@ -69,8 +93,9 @@ public class UriUtilTest extends TestCase {
 		assertThat( parameters.get( "attr2" ), is( "value2" ) );
 	}
 
+	@Test
 	public void testParseQueryWithString() {
-		assertNull( UriUtil.parseQuery( (String)null ) );
+		assertThat( UriUtil.parseQuery( (String)null ), is( nullValue() ) );
 
 		Map<String, String> parameters = UriUtil.parseQuery( "attr1&attr2" );
 		assertThat( parameters.get( "attr1" ), is( "true" ) );
