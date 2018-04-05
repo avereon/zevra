bbded62c1e6975f29774bc91a550423b257a5b41
Mark Soderquist <soderquistmv@ldschurch.org>
2018-04-05 13:32:08 -0600

    Able to lookup resource bundles again

--- source/main/java/com/xeomar/product/ProductBundle.java
+++ source/main/java/com/xeomar/product/ProductBundle.java
@@ -4,6 +4,8 @@ import com.xeomar.util.JavaUtil;
 import com.xeomar.util.LogUtil;
 import org.slf4j.Logger;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.lang.invoke.MethodHandles;
 import java.util.Locale;
 import java.util.MissingResourceException;
@@ -16,23 +18,38 @@ public class ProductBundle {
 
 	private static final Logger log = LogUtil.get( MethodHandles.lookup().lookupClass() );
 
+	private Module module;
+
 	private ClassLoader loader;
 
 	private String prefix;
 
-	public ProductBundle( Class<? extends  Product> product ) {
+	public ProductBundle( Class<? extends Product> product ) {
 		this( product, JavaUtil.getPackagePath( product ) );
 	}
 
-	public ProductBundle( Class<? extends  Product> product, String prefix ) {
+	public ProductBundle( Class<? extends Product> product, String prefix ) {
+		this.module = product.getModule();
 		this.loader = product.getClassLoader();
 		this.prefix = prefix;
+
+//		if( product.getName().contains( "xenon" ) ) {
+//			try {
+//				System.out.println( product.getName() );
+//				InputStream moduleInput = product.getModule().getResourceAsStream( "/META-INF/bundles/action.properties" );
+//				System.out.println( moduleInput != null ? "FOUND MODULE RESOURCE" : "COULD NOT FIND MODULE RESOURCE" );
+//				InputStream classInput = product.getResourceAsStream( "/bundles/action.properties" );
+//				System.out.println( classInput != null ? "FOUND CLASS RESOURCE" : "COULD NOT FIND CLASS RESOURCE" );
+//			} catch( IOException e ) {
+//				e.printStackTrace();
+//			}
+//		}
 	}
 
 	public String getString( String bundleKey, String valueKey, String... values ) {
 		String string = null;
 
-		ResourceBundle bundle = getBundle( prefix + "/" + bundleKey, Locale.getDefault(), loader );
+		ResourceBundle bundle = getBundle( "bundles/" + bundleKey, Locale.getDefault(), module );
 		if( bundle.containsKey( valueKey ) ) string = format( bundle.getString( valueKey ), (Object[])values );
 		if( string == null ) {
 			string = bundleKey + " > " + valueKey;
