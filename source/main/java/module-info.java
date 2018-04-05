7c8bd9f4805ee573282642d02de85907cd5f0599
Mark Soderquist <soderquistmv@ldschurch.org>
2018-04-05 15:46:16 -0600

    Use updated versions of jackson modules

--- source/main/java/module-info.java
+++ source/main/java/module-info.java
@@ -7,7 +7,6 @@ module com.xeomar.razor {
 	requires commons.io;
 	requires com.fasterxml.jackson.databind;
 	requires com.fasterxml.jackson.core;
-	requires jackson.annotations;
 	requires org.slf4j;
 
 	exports com.xeomar.util;
