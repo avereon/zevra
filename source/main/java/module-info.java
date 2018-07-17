f4df9fcf373a60bb5284537e1013c686605c3dc7
Mark Soderquist <soderquistmv@ldschurch.org>
2018-07-17 15:27:04 -0600

    Some dependency updates

--- source/main/java/module-info.java
+++ source/main/java/module-info.java
@@ -4,7 +4,7 @@ module com.xeomar.razor {
 	requires java.management;
 	requires java.xml;
 	requires jdk.management;
-	requires commons.io;
+	requires org.apache.commons.io;
 	requires com.fasterxml.jackson.databind;
 	requires com.fasterxml.jackson.core;
 	requires org.slf4j;
