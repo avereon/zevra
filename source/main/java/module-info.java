e657c9789038346cf7cf303a486c27cd35b82f3c
Mark Soderquist <soderquistmv@churchofjesuschrist.org>
2019-10-14 14:08:37 -0600

    Junit5 compliant tests

--- source/main/java/module-info.java
+++ source/main/java/module-info.java
@@ -1,16 +1,17 @@
-open module com.avereon.zevra {
+module com.avereon.zevra {
 
 	requires java.logging;
 	requires java.management;
 	requires java.xml;
 	requires jdk.management;
+	requires com.fasterxml.jackson.annotation;
 	requires com.fasterxml.jackson.databind;
 	requires com.fasterxml.jackson.core;
 	requires org.slf4j;
-	requires com.fasterxml.jackson.annotation;
 
 	exports com.avereon.product;
 	exports com.avereon.settings;
 	exports com.avereon.undo;
 	exports com.avereon.util;
+
 }
