e62da9ecc6a5f2bd88be625c26e563230a405237
Mark Soderquist <soderquistmv@churchofjesuschrist.org>
2019-10-18 10:39:07 -0600

    Deprecate use of Configurable for later removal

--- source/main/java/com/avereon/util/Configurable.java
+++ source/main/java/com/avereon/util/Configurable.java
@@ -2,10 +2,13 @@ package com.avereon.util;
 
 import com.avereon.settings.Settings;
 
+@Deprecated
 public interface Configurable {
 
+	@Deprecated
 	void setSettings( Settings settings );
 
+	@Deprecated
 	Settings getSettings();
 
 }
