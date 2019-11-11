c77efdf35df3ba3fe56380d19b0a5b7934c02092
Mark Soderquist <soderquistmv@churchofjesuschrist.org>
2019-11-11 08:41:36 -0700

    Reduce complexity of Controllable interface

--- source/main/java/com/avereon/util/Controllable.java
+++ source/main/java/com/avereon/util/Controllable.java
@@ -1,10 +1,19 @@
 package com.avereon.util;
 
 /**
- * The Controllable interface represents classes that can be "controlled" by
- * starting and stopping.
+ * The Controllable interface provides an API for classes that are "controlled"
+ * by {@link #start()} and {@link #stop()} methods.
+ * <p/>
+ * Implementations of this interface are expected to be synchronous. This means
+ * that when execution exits the {@link #start()} method the Controllable is
+ * started and this {@link #isRunning()} method will return true. Similarly,
+ * when execution exits the {@link #stop()} method the Controllable is stopped
+ * and the {@link #isRunning()} method will return false.
+ * <p/>
+ * No expectation whether {@link #start()} can be called again after
+ * {@link #stop()} is called is not defined by this interface.
  *
- * @param <T>
+ * @param <T> The type of the controllable
  */
 @SuppressWarnings( { "unused", "UnusedReturnValue" } )
 public interface Controllable<T> {
@@ -13,14 +22,6 @@ public interface Controllable<T> {
 
 	T start();
 
-//	T awaitStart( long timeout, TimeUnit unit ) throws InterruptedException;
-//
-//	T restart();
-//
-//	T awaitRestart( long timeout, TimeUnit unit ) throws InterruptedException;
-
 	T stop();
-
-//	T awaitStop( long timeout, TimeUnit unit ) throws InterruptedException;
 
 }
