6a52a841be4294912fcee0eaaade635c3e1d5d02
Mark Soderquist <mark@soderquist.net>
2019-12-23 20:04:46 -0700

    New event model

--- source/main/java/com/avereon/event/Event.java
+++ source/main/java/com/avereon/event/Event.java
@@ -1,6 +1,5 @@
 package com.avereon.event;
 
-import java.util.Collection;
 import java.util.EventObject;
 
 public class Event extends EventObject {
@@ -22,12 +21,6 @@ public class Event extends EventObject {
 
 	public EventType<? extends Event> getEventType() {
 		return type;
-	}
-
-	@SuppressWarnings( "unchecked" )
-	public <E extends Event> E fire( final Collection<? extends EventHandler<E>> handlers ) {
-		handlers.forEach( (h) -> h.handle( (E)this ) );
-		return (E)this;
 	}
 
 }
