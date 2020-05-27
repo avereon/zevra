4d6ee0946df00d8693677fa5c5385862dee4f000
Mark Soderquist <mark@soderquist.net>
2020-05-27 17:06:23 -0600

    Add Identified interface

---
+++ source/main/java/com/avereon/skill/Identified.java
@@ -0,0 +1,12 @@
+package com.avereon.skill;
+
+public interface Identified {
+
+	// NOTE Do not change this key with a plan to migrate the values
+	String KEY = "id";
+
+	String getId();
+
+	void setId( String id );
+
+}
