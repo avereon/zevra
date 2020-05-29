b559c89a0c71b80df88738623abe625fc6e354cf
Mark Soderquist <mark@soderquist.net>
2020-05-29 11:23:20 -0600

    Rework Identity

--- source/main/java/com/avereon/skill/Identity.java
+++ source/main/java/com/avereon/skill/Identity.java
@@ -1,12 +1,13 @@
 package com.avereon.skill;
 
-public interface Identified {
+public interface Identity {
 
 	// NOTE Do not change the value of this key without a plan to migrate persisted values
 	String KEY = "id";
 
+	// TODO Rename to getTag()
+	// TODO Rename to getIdent()
+	// TODO Rename to getIdKey()
 	String getProductId();
-
-	void setProductId( String id );
 
 }
---
+++ source/main/java/com/avereon/skill/WritableIdentity.java
@@ -0,0 +1,7 @@
+package com.avereon.skill;
+
+public interface WritableIdentity extends Identity {
+
+	void setProductId( String id );
+
+}
