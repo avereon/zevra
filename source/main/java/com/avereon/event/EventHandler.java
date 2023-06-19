47c2225fd054eea0546a0e194ec748349b43a477
msoderquist <msoderquist@mastercontrol.com>
2023-06-19 16:24:41 -0600

    EventHandler should be a FunctionalInterface

--- source/main/java/com/avereon/event/EventHandler.java
+++ source/main/java/com/avereon/event/EventHandler.java
@@ -2,6 +2,7 @@ package com.avereon.event;
 
 import java.util.EventListener;
 
+@FunctionalInterface
 public interface EventHandler<T extends Event> extends EventListener {
 
 	void handle( T event );
