9308dc40d3dabeb1e9e847623d59504a40ea11e0
msoderquist <msoderquist@mastercontrol.com>
2023-11-10 08:33:43 -0700

    Update thread name comparator

--- source/main/java/com/avereon/util/ThreadInfoNameComparator.java
+++ source/main/java/com/avereon/util/ThreadInfoNameComparator.java
@@ -7,6 +7,13 @@ public class ThreadInfoNameComparator implements Comparator<ThreadInfo> {
 
 	@Override
 	public int compare( ThreadInfo threadInfo1, ThreadInfo threadInfo2 ) {
+		String name1 = threadInfo1 == null ? null : threadInfo1.getThreadName();
+		String name2 = threadInfo2 == null ? null : threadInfo2.getThreadName();
+
+		if( name1 == null && name2 == null ) return 0;
+		if( name1 == null ) return -1;
+		if( name2 == null ) return 1;
+
 		return threadInfo1.getThreadName().compareToIgnoreCase( threadInfo2.getThreadName() );
 	}
 
