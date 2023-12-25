645fd14b7c3121d185287a8770347f59bcdc09e4
msoderquist <msoderquist@mastercontrol.com>
2023-12-25 15:30:19 -0700

    Improve readability in FuzzySearch class
    Modified some descriptions and renamed a constant in the FuzzySearch class to improve readability. The constant 'CUTOFF' was renamed to 'DEFAULT_CUTOFF' to better reflect its purpose. Word choice in the comments was also adjusted to enhance the explanation of how fuzzy search works.

--- source/main/java/com/avereon/index/FuzzySearch.java
+++ source/main/java/com/avereon/index/FuzzySearch.java
@@ -8,22 +8,22 @@ import java.util.stream.Collectors;
 /**
  * The fuzzy search implementation will find hits based off the Levenshtein
  * distance algorithm to find matches of the search term on the index. This will
- * result in an ordered list of hits but it is up to the requester to sort the
+ * result in an ordered list of hits, but it is up to the requester to sort the
  * resulting hit documents.
  */
 public class FuzzySearch implements Search {
 
 	/**
-	 * Levenshtein distance cutoff distance (0-100). Using zero would return
-	 * everything in the index (probably not what we want) and using 100 would
+	 * Levenshtein distance cutoff value (0-100). Using zero would return
+	 * everything in the index (probably not desired) and using 100 would
 	 * return only exact matches.
 	 */
-	private static final int CUTOFF = 80;
+	private static final int DEFAULT_CUTOFF = 80;
 
 	private final int cutoff;
 
 	public FuzzySearch() {
-		this( CUTOFF );
+		this( DEFAULT_CUTOFF );
 	}
 
 	public FuzzySearch( int cutoff ) {
