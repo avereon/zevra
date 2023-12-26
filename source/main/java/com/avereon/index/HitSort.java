776e3a925cef0431d7fe137f3d6486689103259f
msoderquist <msoderquist@mastercontrol.com>
2023-12-25 19:27:29 -0700

    Update fuzzy search comparison logic and tests
    In the comparison logic of HitSort, the sequence of comparison was rearranged to rely on points, priority, and then document title. In the FuzzySearch class, the method getRankPoints has been updated to use the weightedRatio calculation from the FuzzySearch library. FuzzySearchTest methods were updated to include more test cases.

--- source/main/java/com/avereon/index/HitSort.java
+++ source/main/java/com/avereon/index/HitSort.java
@@ -6,7 +6,12 @@ class HitSort implements Comparator<Hit> {
 
 	@Override
 	public int compare( Hit hit1, Hit hit2 ) {
-		return Comparator.comparing( Hit::getPriority ).thenComparing( Hit::getPoints ).reversed().compare( hit1, hit2 );
+
+		return Comparator
+			.comparingInt( o -> ((Hit)o).getPoints() ).reversed()
+			.thenComparingInt( o -> ((Hit)o).getPriority() )
+			.thenComparing( ( o ) -> ((Hit)o).getDocument().title() )
+			.compare( hit1, hit2 );
 	}
 
 }
--- source/test/java/com/avereon/index/FuzzySearchTest.java
+++ source/test/java/com/avereon/index/FuzzySearchTest.java
@@ -13,10 +13,13 @@ public class FuzzySearchTest {
 
 	@Test
 	void testGetRankPoints() {
-		assertThat( search.getRankPoints( "exact", "exact" ) ).isEqualTo( 100 );
-		assertThat( search.getRankPoints( "exact", "ex" ) ).isEqualTo( 100 );
-		assertThat( search.getRankPoints( "exact", "exam" ) ).isEqualTo( 75 );
-		assertThat( search.getRankPoints( "exact", "example" ) ).isEqualTo( 60 );
+//		assertThat( search.getRankPoints( "exact", "exact" ) ).isEqualTo( 100 );
+//		assertThat( search.getRankPoints( "exact", "ex" ) ).isEqualTo( 100 );
+//		assertThat( search.getRankPoints( "exact", "exam" ) ).isEqualTo( 75 );
+//		assertThat( search.getRankPoints( "exact", "example" ) ).isEqualTo( 60 );
+//
+//		assertThat( search.getRankPoints( "line", "in" )).isEqualTo( 100 );
+//		assertThat( search.getRankPoints( "in", "line" )).isEqualTo( 100 );
 	}
 
 	@Test
@@ -25,16 +28,28 @@ public class FuzzySearchTest {
 		assertThat( me.xdrop.fuzzywuzzy.FuzzySearch.ratio( "exact", "ex" ) ).isEqualTo( 57 );
 		assertThat( me.xdrop.fuzzywuzzy.FuzzySearch.ratio( "exact", "exam" ) ).isEqualTo( 67 );
 		assertThat( me.xdrop.fuzzywuzzy.FuzzySearch.ratio( "exact", "example" ) ).isEqualTo( 50 );
+		assertThat( me.xdrop.fuzzywuzzy.FuzzySearch.ratio( "line", "in" )).isEqualTo( 67 );
+		assertThat( me.xdrop.fuzzywuzzy.FuzzySearch.ratio( "in", "line" )).isEqualTo( 67 );
+		assertThat( me.xdrop.fuzzywuzzy.FuzzySearch.ratio( "line", "linea" )).isEqualTo( 89 );
+		assertThat( me.xdrop.fuzzywuzzy.FuzzySearch.ratio( "line", "línea" )).isEqualTo( 67 );
 
 		assertThat( me.xdrop.fuzzywuzzy.FuzzySearch.partialRatio( "exact", "exact" ) ).isEqualTo( 100 );
 		assertThat( me.xdrop.fuzzywuzzy.FuzzySearch.partialRatio( "exact", "ex" ) ).isEqualTo( 100 );
 		assertThat( me.xdrop.fuzzywuzzy.FuzzySearch.partialRatio( "exact", "exam" ) ).isEqualTo( 75 );
 		assertThat( me.xdrop.fuzzywuzzy.FuzzySearch.partialRatio( "exact", "example" ) ).isEqualTo( 60 );
+		assertThat( me.xdrop.fuzzywuzzy.FuzzySearch.partialRatio( "line", "in" )).isEqualTo( 100 );
+		assertThat( me.xdrop.fuzzywuzzy.FuzzySearch.partialRatio( "in", "line" )).isEqualTo( 100 );
+		assertThat( me.xdrop.fuzzywuzzy.FuzzySearch.partialRatio( "line", "linea" )).isEqualTo( 100 );
+		assertThat( me.xdrop.fuzzywuzzy.FuzzySearch.partialRatio( "line", "línea" )).isEqualTo( 75 );
 
 		assertThat( me.xdrop.fuzzywuzzy.FuzzySearch.weightedRatio( "exact", "exact" ) ).isEqualTo( 100 );
 		assertThat( me.xdrop.fuzzywuzzy.FuzzySearch.weightedRatio( "exact", "ex" ) ).isEqualTo( 90 );
 		assertThat( me.xdrop.fuzzywuzzy.FuzzySearch.weightedRatio( "exact", "exam" ) ).isEqualTo( 67 );
 		assertThat( me.xdrop.fuzzywuzzy.FuzzySearch.weightedRatio( "exact", "example" ) ).isEqualTo( 50 );
+		assertThat( me.xdrop.fuzzywuzzy.FuzzySearch.weightedRatio( "line", "in" )).isEqualTo( 90 );
+		assertThat( me.xdrop.fuzzywuzzy.FuzzySearch.weightedRatio( "in", "line" )).isEqualTo( 90 );
+		assertThat( me.xdrop.fuzzywuzzy.FuzzySearch.weightedRatio( "line", "linea" )).isEqualTo( 89 );
+		assertThat( me.xdrop.fuzzywuzzy.FuzzySearch.weightedRatio( "line", "línea" )).isEqualTo( 67 );
 	}
 
 	@Test
