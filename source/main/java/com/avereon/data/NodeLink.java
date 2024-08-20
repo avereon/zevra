38c03f47754629b5c6049af83f587370875b4f68
msoderquist <msoderquist@mastercontrol.com>
2024-08-20 12:01:29 -0600

    Cleanup

--- source/main/java/com/avereon/data/NodeLink.java
+++ source/main/java/com/avereon/data/NodeLink.java
@@ -1,15 +1,14 @@
 package com.avereon.data;
 
+import lombok.Getter;
+
+@Getter
 public class NodeLink<T extends Node> extends IdNode {
 
 	private final T node;
 
 	public NodeLink( T node ) {
 		this.node = node;
-	}
-
-	public T getNode() {
-		return node;
 	}
 
 	@Override
